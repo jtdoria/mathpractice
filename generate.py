9d3546d83334dabe975219a3ce6e2bcf9053658c
Joe Doria <joe.t.doria@gmail.com>
2020-11-18 13:12:29 -0800

    Add expression generator for arithmetic order of operations

--- generate.py
+++ generate.py
@@ -16,3 +16,35 @@ def gen_arithmetic(operation, diff):
     op2 = random.randint(0, upper_bound)
     expr = f"{op1} {operation} {op2}"
     return expr
+
+
+def gen_order_of_operations(diff):
+    """Function to generate expressions of various length with the arithmetic operations of addition, subtraction,
+    multiplication, and division.
+
+    Parameters
+        diff int: difficulty level 0, 1, or 2.
+    Return
+        expr string: expression string with latex formatting
+    """
+    latex_bank = {
+        'operations': (
+            '+',
+            '-',
+            '*',
+            '/',
+        ),
+    }
+
+    num_of_operations = 3 + diff
+    num_of_operands = num_of_operations + 1
+
+    operations = [operation for operation in random.choices(latex_bank['operations'], k=num_of_operations)]
+    operands = [str(random.randint(0, 10)) for _ in range(num_of_operands)]
+
+    expression_list = operands[:]
+    for i in range(num_of_operations):
+        expression_list.insert(2 * i + 1, operations[i])
+
+    expression_string = ' '.join(expression_list)
+    return expression_string

10d329de308f86c8be1a2807f47e46e87117a38d
Joe Doria <joe.t.doria@gmail.com>
2020-12-10 21:00:37 -0800

    Change determine next step logic to use leaf nodes at any depth level

--- evaluate.py
+++ evaluate.py
@@ -12,12 +12,11 @@ logging.config.dictConfig(lc.config_dict)
 logger = logging.getLogger('my_logger')
 
 
-def collect_leaf_nodes(node, lf_nds=None, level=0):
+def collect_leaf_nodes(node, lf_nds=None):
     """
     Arguments:
         node: (tree.Node object) to provide starting node from which to begin evaluation.
         lf_nds: (list) to contain the leaf nodes.
-        level: (int) to keep track of tree depth level.
     Returns:
         leaf_nodes: (list of tuples) list containing tuples of all leaf nodes and their respective depth level in the
         form (node, level).
@@ -28,78 +27,59 @@ def collect_leaf_nodes(node, lf_nds=None, level=0):
     if node.get_children():
         num_of_children = len(node.get_children())
         for i in range(num_of_children):
-            collect_leaf_nodes(node=node.get_child(i), lf_nds=lf_nds, level=level + 1)
+            collect_leaf_nodes(node=node.get_child(i), lf_nds=lf_nds)
             logger.debug(f"leaf_nodes so far: {lf_nds}")
-            # TODO: Subtraction handling.
-            # if (i != num_of_children - 1) and isinstance(node.get_child(i + 1).get_cargo(), defn.Subtract):
-            #     result += "-"
-            # if (i != num_of_children - 1) and not isinstance(node.get_child(i + 1).get_cargo(), defn.Subtract):
-            #     result += str(node.get_cargo())
     else:
-        logger.debug(f"appending: '{node, level}'")
-        lf_nds.append((node, level))
+        logger.debug(f"appending: '{node}'")
+        lf_nds.append(node)
     logger.debug(f"collect_leaf_nodes returned: {lf_nds}")
     return lf_nds
-
-
-def get_deepest_leaf_nodes(lf_nds):
-    """
-    Function to return a list of leaf nodes at the deepest level of the tree.
-
-    :param lf_nds: list of tuples containing a leaf node and its level in the form [(<node>, <level>),].
-    :return: list of leaf nodes at the deepest level of the tree.
-
-    :test:
-    a = [(1, 2),(3, 1),(18, 75), (6, 14), (89, 75)]
-    b = get_deepest_leaf_nodes(a)
-    print(b) # should print [(18, 75), (89, 75)]
-    """
-    depth_level = 0
-    deepest_leaf_nodes = []
-
-    for lf in lf_nds:
-        if lf[1] == depth_level:
-            deepest_leaf_nodes.append(lf[0])
-        elif lf[1] > depth_level:
-            depth_level = int(lf[1])
-            deepest_leaf_nodes.clear()
-            deepest_leaf_nodes.append(lf[0])
-
-    return deepest_leaf_nodes
 
 
 def determine_next_step(lf_nds):
     """
     Function to determine the next step to execute from a list of leaf nodes.
 
-    :param lf_nds: list of leaf nodes expected to come from collect_leaf_nodes.
-    :return step_root_node: root node of the next step to be performed
+    Arguments
+        lf_nds: list of leaf nodes expected to come from collect_leaf_nodes
+    Return
+        candidate_node: root node of the subtree representing the next step to be executed
     """
     logger.debug(f"lf_nds = {lf_nds}")
-    dp_lf_nds = get_deepest_leaf_nodes(lf_nds)
-    logger.debug(f"dp_lf_nds = {dp_lf_nds}")
-    still_looking = True
-    step_root_node = None
-    index = 0
-    siblings_found = 0
 
-    while still_looking:
-        current_leaf = dp_lf_nds[index]
-        siblings = current_leaf.get_parent().get_children()
-        logger.debug(f"index: {index}")
-        logger.debug(f"current_leaf: {current_leaf}")
-        logger.debug(f"siblings: {siblings}")
-        for sibling in siblings:
-            for lf in dp_lf_nds:
-                if id(sibling) == id(lf):  # TODO: use UUID instead of mem address id()
-                    siblings_found += 1
-                    logger.debug(f"sibling found, siblings_found = {siblings_found}")
-                    if siblings_found == len(siblings):
-                        step_root_node = current_leaf.get_parent()
-                        still_looking = False
-        index += 1
-    logger.debug(f"step_root_node: {step_root_node} with children: {step_root_node.get_children()}")
-    return step_root_node
+    order_priority = {
+        '*': 0,
+        '/': 0,
+        '+': 1,
+        '-': 1,
+    }
+
+    candidate_node = None
+
+    while lf_nds:
+
+        current_leaf = lf_nds[0]
+        current_parent = current_leaf.get_parent()
+        current_siblings = current_parent.get_children()
+
+        # check if there are any subtractions in current_siblings
+        # if yes, then replace each subtraction with it's child
+        for i in range(len(current_siblings)):
+            if isinstance(current_siblings[i].get_cargo(), defn.Subtract):
+                current_siblings[i] = current_siblings[i].get_child()
+
+        if (set(current_siblings) & set(lf_nds)) == set(current_siblings):
+            if candidate_node:
+                if order_priority[str(current_parent.get_cargo())] < order_priority[str(candidate_node.get_cargo())]:
+                    candidate_node = current_parent
+            else:
+                candidate_node = current_parent
+
+        for sibling in (set(current_siblings) & set(lf_nds)):
+            lf_nds.remove(sibling)
+
+    logger.debug(f"returning candidate_node: {candidate_node} with children: {candidate_node.get_children()}")
+    return candidate_node
 
 
 def evaluate_step(node):
